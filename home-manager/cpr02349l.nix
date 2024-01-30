a5d846c68436b2e716f827277a1301b89da13455
Tom Neudoerffer <tneudoerffer@ottomotors.com>
2024-01-30 15:45:31 -0500

    add nixGL for non-nixos/darwin gui programs

--- home-manager/cpr02349l.nix
+++ home-manager/cpr02349l.nix
@@ -1,7 +1,32 @@
-{ lib, ... }: {
+{ lib, pkgs, stdenv, ... }:
+let
+  alacritty-bin = pkgs.writeScriptBin "alacritty-bin" ''
+    #!/bin/sh
+    exec ${pkgs.nixgl.nixGLIntel}/bin/nixGLIntel ${pkgs.alacritty}/bin/alacritty
+  '';
+  alacritty-desktop = stdenv.mkDerivation {
+    desktopItems = [
+      (pkgs.makeDesktopItem {
+        type = "Application";
+        name = "nixGL Alacritty";
+        desktopName = "nixGL Alacritty";
+        exec = "${alacritty-bin}/bin/alacritty-bin %U";
+        genericName = "Terminal";
+        icon = builtins.fetchurl {
+          url = "https://raw.githubusercontent.com/alacritty/alacritty/master/extra/logo/alacritty-term.svg";
+          sha256 = "sha256:1ljxxdljj76a3vqldzssjg5j45ljazk7ismci4cd5ikyvb89m3b5";
+        };
+      })
+    ];
+    installPhase = "copyDesktopItems";
+  };
+in
+{
   home = {
     username = "tneudoerffer";
     homeDirectory = "/home/tneudoerffer";
+    packages =
+      (with pkgs.nixgl; [ nixGLIntel ]) ++ [ alacritty-desktop ];
   };
 
   programs = {
