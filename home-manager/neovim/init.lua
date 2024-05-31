e164fbda413d7acb8ed1a3569a0e706995b01796
Tom Neudoerffer <tom@neudoerffer.com>
2024-05-30 21:31:02 -0400

    no more undefined global vim

--- home-manager/neovim/init.lua
+++ home-manager/neovim/init.lua
@@ -608,6 +608,7 @@ local servers = {
     Lua = {
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
+      diagnostics = { globals = { 'vim' } }
       -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
       -- diagnostics = { disable = { 'missing-fields' } },
     },
