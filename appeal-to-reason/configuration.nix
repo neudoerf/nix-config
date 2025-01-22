8c04f1e4e6287090f8b16d0fcecf7e22c39d044b
Tom Neudoerffer <tom@neudoerffer.com>
2025-01-22 11:27:57 -0500

    centralize vm config

--- appeal-to-reason/configuration.nix
+++ appeal-to-reason/configuration.nix
@@ -1,20 +1,14 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{ pkgs, ... }:
+{ ... }:
 {
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./../defaults/vm.nix
     ./../services/caddy.nix
   ];
-
-  boot = {
-    # Bootloader.
-    loader.systemd-boot.enable = true;
-    loader.efi.canTouchEfiVariables = true;
-    kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 0;
-  };
 
   networking = {
     hostName = "appeal-to-reason";
@@ -25,51 +19,6 @@
     ];
   };
 
-  time.timeZone = "America/Toronto";
-
-  i18n.defaultLocale = "en_CA.UTF-8";
-
-  programs.fish.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.neudoerf = {
-    isNormalUser = true;
-    description = "Tom Neudoerffer";
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-    ];
-    # packages = with pkgs; [];
-    shell = pkgs.fish;
-  };
-
-  # List packages installed in system profile. To search, run:
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-  environment.systemPackages = with pkgs; [
-    git
-    vim
-    wget
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-  virtualisation.containers.enable = true;
-  services = {
-    openssh.enable = true;
-    qemuGuest.enable = true;
-  };
-
-  security.sudo.wheelNeedsPassword = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
---
+++ defaults/vm.nix
@@ -0,0 +1,50 @@
+{ pkgs, ... }:
+{
+  boot = {
+    # Bootloader.
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 0;
+  };
+
+  time.timeZone = "America/Toronto";
+
+  i18n.defaultLocale = "en_CA.UTF-8";
+
+  programs.fish.enable = true;
+
+  users.users.neudoerf = {
+    isNormalUser = true;
+    description = "Tom Neudoerffer";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIyPIcsMhs7ArU4sdxwoI9RxUP0o69UDXZ8b4X/iPeJf"
+    ];
+    shell = pkgs.fish;
+  };
+
+  # List packages installed in system profile. To search, run:
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    wget
+  ];
+
+  virtualisation.containers.enable = true;
+
+  services = {
+    openssh.enable = true;
+    qemuGuest.enable = true;
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+}
